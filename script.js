69e140e5fdf5c24caf7f02c10c23e088b9ef7f8b
Milad <mtz.milad@gmail.com>
2023-10-15 17:00:26 +0200

    #Installing NodJS Packages.

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add interactivity to the "Start Quiz" button
+const startQuizButton = document.getElementById("start-quiz");
+startQuizButton.addEventListener("click", () => {
+    alert("Quiz started! Good luck!");
+});
